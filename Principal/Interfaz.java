9f848441c853d46c21e774b510e006ab8d5437cd
Daniel Fuelpaz <efuelpaz8870@uta.edu.ec>
2022-06-28 12:17:28 -0500

    El cascaron

--- Principal/Interfaz.java
+++ Principal/Interfaz.java
@@ -1,13 +1,34 @@
 package Principal;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JToggleButton;
 
 public class Interfaz implements ActionListener {
 
+    public JFrame Fbase = new JFrame("Mostrar Imagen");
+    public JToggleButton opcion1 = new JToggleButton("Buscar");
+    public JToggleButton opcion2 = new JToggleButton("Buscar");
+    public JToggleButton opcion3 = new JToggleButton("Buscar");
+    public JToggleButton opcion4 = new JToggleButton("Buscar");
+
+    public void initialize(){
+        Fbase.setSize(600, 600);
+        Fbase.setLocation(0, 0);
+        Fbase.setBackground(Color.CYAN);
+        Fbase.setLayout(new FlowLayout());
+    }
+    
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+    }
+    public static void main(String[] args) {
+        Interfaz i = new Interfaz();
+        i.initialize();
     }
 
 }
