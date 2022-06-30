1a9f6c91cc9e67c31da2ab0baa47d123ba607295
Daniel Fuelpaz <efuelpaz8870@uta.edu.ec>
2022-06-30 11:46:14 -0500

    hora de que explote jajaj

--- Ncedula/Ncedula.java
+++ Ncedula/Ncedula.java
@@ -1,5 +1,24 @@
 package Ncedula;
 
-public class Ncedula {
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Ncedula implements ActionListener {
+
+    private JPanel panelcedula(JLabel a) {
+        JPanel Jp1 = new JPanel();
+        Jp1.add(a);
+        Jp1.setBounds(100, 100, 375, 150);
+        Jp1.setBackground(Color.LIGHT_GRAY);
+        return Jp1;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 }
