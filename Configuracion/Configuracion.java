72d3b3afd462fe9c85a063a29fc0eb4545cd6d47
Daniel Fuelpaz <efuelpaz8870@uta.edu.ec>
2022-07-11 10:15:45 -0500

    borrando lineas que estaban con cosas que no se usaban

--- Configuracion/Configuracion.java
+++ Configuracion/Configuracion.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Configuracion;
 
 import Conexion.Conexion;
@@ -25,62 +20,64 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author g4to1
  */
-public class Configuracion implements ActionListener{
-    public JFrame frame=new JFrame("Configuracion");
-    public JLabel op1=new JLabel("Seleccione la Opcion");
-    public JLabel op2=new JLabel("Nombre Ciudad");
-    public JLabel op3=new JLabel("Nombre Provincia");
-    public JComboBox cb1=new JComboBox();
-    public JTextField txtop2=new JTextField();
-    public JButton guardar=new JButton("Guardar");
-    
+public class Configuracion implements ActionListener {
+
+    public JFrame frame = new JFrame("Configuracion");
+    public JLabel op1 = new JLabel("Seleccione la Opcion");
+    public JLabel op2 = new JLabel("Nombre Ciudad");
+    public JLabel op3 = new JLabel("Nombre Provincia");
+    public JComboBox cb1 = new JComboBox();
+    public JTextField txtop2 = new JTextField();
+    public JButton guardar = new JButton("Guardar");
+
     DefaultTableModel modelo = new DefaultTableModel();
-    public void initialize(){
-        frame.setBounds(750,300, 400, 400);
+
+    public void initialize() {
+        frame.setBounds(750, 300, 400, 400);
         frame.add(this.op1);
         frame.setLayout(null);
         op1.setBounds(15, 30, 215, 15);
         frame.add(this.cb1);
-        cb1.setBounds(150,30,200,20);
+        cb1.setBounds(150, 30, 200, 20);
         frame.add(this.guardar);
-        guardar.setBounds(135,200,100,20);
-        
+        guardar.setBounds(135, 200, 100, 20);
+
         this.cb1.addItem("Opcion");
         this.cb1.addItem("Ciudad");
         this.cb1.addItem("Provincia");
-        
-        this.cb1.addActionListener(new ActionListener(){
+
+        this.cb1.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent a){
-                if(cb1.getSelectedItem().toString()=="Ciudad"){
+            public void actionPerformed(ActionEvent a) {
+                if (cb1.getSelectedItem().toString() == "Ciudad") {
                     op3.show(false);
                     op2.show(true);
                     txtop2.show(true);
                     frame.add(op2);
                     op2.setBounds(15, 70, 215, 15);
                     frame.add(txtop2);
-                    txtop2.setBounds(150,70,200,20);
-                }else if(cb1.getSelectedItem().toString()=="Provincia"){
+                    txtop2.setBounds(150, 70, 200, 20);
+                } else if (cb1.getSelectedItem().toString() == "Provincia") {
                     op2.show(false);
                     op3.show(true);
                     txtop2.show(true);
-                    frame.add(op3);       
+                    frame.add(op3);
                     op3.setBounds(15, 70, 215, 15);
                     frame.add(txtop2);
-                    txtop2.setBounds(150,70,200,20);
-                }else{
+                    txtop2.setBounds(150, 70, 200, 20);
+                } else {
                     op3.show(false);
                     op2.show(false);
                     txtop2.show(false);
                 }
-                
-            }   
+
+            }
         });
 
-        this.guardar.addActionListener(new ActionListener(){
+        this.guardar.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent g){
-                if((cb1.getSelectedItem().toString()=="Ciudad")&&(txtop2.getText()!="")){
+            public void actionPerformed(ActionEvent g) {
+                if ((cb1.getSelectedItem().toString() == "Ciudad") && (txtop2.getText() != "")) {
                     PreparedStatement ps = null;
                     Conexion objCon = new Conexion();
                     Connection conn = objCon.getConexion();
@@ -92,8 +89,8 @@ public class Configuracion implements ActionListener{
                     } catch (SQLException ex) {
                         Logger.getLogger(Configuracion.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                    
-                }else if((cb1.getSelectedItem().toString()=="Provincia")&&(txtop2.getText()!="")){
+
+                } else if ((cb1.getSelectedItem().toString() == "Provincia") && (txtop2.getText() != "")) {
                     PreparedStatement ps = null;
                     Conexion objCon = new Conexion();
                     Connection conn = objCon.getConexion();
@@ -105,7 +102,7 @@ public class Configuracion implements ActionListener{
                     } catch (SQLException ex) {
                         Logger.getLogger(Configuracion.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                }else{
+                } else {
                     JOptionPane.showMessageDialog(null, "No se ha seleccionado ningun item");
                 }
             }
@@ -113,13 +110,15 @@ public class Configuracion implements ActionListener{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.show();
     }
+
     @Override
     public void actionPerformed(ActionEvent ae) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
     public static void main(String[] args) {
-        Configuracion cf=new Configuracion();
+        Configuracion cf = new Configuracion();
         cf.initialize();
     }
-    
+
 }
--- Opcion3/MySQLComandos.java
+++ Opcion3/MySQLComandos.java
@@ -1,7 +1,10 @@
 package Opcion3;
 
+import Conexion.Conexion;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MySQLComandos {
     private String instruccion;
@@ -30,6 +33,19 @@ public class MySQLComandos {
 
     public void setRs(ResultSet rs) {
         this.rs = rs;
+    }
+    
+     public ResultSet consultarLoguin(String u, String clave) throws SQLException
+    {
+        Conexion c = new Conexion();
+        String sql="Select * from usuarios where usuario=? and clave=?";
+        Connection co = c.getConexion();
+        PreparedStatement ps;
+        ps = co.prepareStatement(sql);
+        ps.setString(1,u);
+        ps.setString(2, clave);
+        ResultSet rs = ps.executeQuery();
+        return rs;
     }
 
 }
